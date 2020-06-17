aaa4177219be8b3d0967b0a92114dbcb4beab72a
Koran Agan <koranagan@gmail.com>
2020-06-17 11:38:24 -0400

    add aic-photography to portfolio

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -4,11 +4,11 @@ import { Link } from "react-router-dom";
 export default function Portfolio() {
   return (
     <div className="portfolio">
-    <a href="http://gary-winogrand.netlify.app"><img src="../assets/winogrand.jpg"></img></a>
-    <a href="https://vietnaam88.netlify.app/"><img src="../assets/vietnaam.jpg"></img></a>
-    <a href="http://clothespicker.surge.sh/"><img src="../assets/clothes-picker.jpg"></img></a>
-    <a href="http://dominiquecarre.fr/"><img src="../assets/carre.jpg"></img></a>
-    <a href="http://my-recipebook.surge.sh/"><img src="../assets/recipe-book.jpg"></img></a>
+    <a href="http://aic-photography.surge.sh/" target="_blank"><img src="../assets/aic-photography.jpg"></img></a>
+    <a href="https://vietnaam88.netlify.app/" target="_blank"><img src="../assets/vietnaam.jpg"></img></a>
+    <a href="http://clothespicker.surge.sh/" target="_blank"><img src="../assets/clothes-picker.jpg"></img></a>
+    <a href="http://dominiquecarre.fr/" target="_blank"><img src="../assets/carre.jpg"></img></a>
+    <a href="http://my-recipebook.surge.sh/" target="_blank"><img src="../assets/recipe-book.jpg"></img></a>
     </div>
   );
 }
