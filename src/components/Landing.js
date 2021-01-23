02216e14c72830072345bf869fe1dfdbe13fbd3c
Koran Agan <koranagan@gmail.com>
2021-01-22 22:07:52 -0500

    add lara project

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -14,10 +14,49 @@ export default function Landing() {
        <h1>
           Current Projects
           </h1>
+       
+          <div className='project lara'>
+         <h2>
+             <a href='https://larabello.com'>LaraBello.com</a>
+          </h2>
+          <h3>Stack: ReactJS, NextJS, API (Prismic CMS), JavaScript, CSS</h3>
+          <div className="logos small">
+            <img src="../assets/three.png"></img>
+            <img src="../assets/reactJS.png"></img>
+            <img src="../assets/nextJS.png"></img>
+            <img src="../assets/prismic-io.jpg"></img>
+          </div>
+          <div className='github-logo'>
+             <h3>Source:</h3>
+            <a href='https://github.com/agan-k/lara'>
+               <img src='../assets/github.png' />
+            </a>
+          </div>
+
+          <div className='screen-shots'>
+             <div className='desktop'>
+               <img src='../assets/screen_shots/lara_desktop1.jpg'/>
+               <img src='../assets/screen_shots/lara_desktop2.jpg'/>
+             </div>
+          </div>
+       </div>
+
        <div className='project raj'>
          <h2>
              <a href='https://rajivjayaweera.com'>RajivJayaweera.com</a>
-         </h2>
+          </h2>
+          <h3>Stack: ReactJS, CSS</h3>
+          <div className="logos small">
+            <img src="../assets/three.png"></img>
+            <img src="../assets/reactJS.png"></img>
+          </div>
+          <div className='github-logo'>
+             <h3>Source:</h3>
+            <a href='https://github.com/agan-k/raj'>
+               <img src='../assets/github.png' height='50' />
+            </a>
+          </div>
+
           <div className='screen-shots'>
              <div className='desktop'>
                <img src='../assets/screen_shots/raj_desktop.jpg'/>
@@ -34,8 +73,20 @@ export default function Landing() {
 
        <div className='project aic'>
          <h2>
-         <a href='https://aic-photography-collection.info'>AIC-Photography-Collection.info</a>
-         </h2>
+            <a href='https://aic-photography-collection.info'>AIC-Photography-Collection.info</a>
+          </h2>
+          <h3>Stack: ReactJS, API, CSS</h3>
+          <div className="logos small">
+            <img src="../assets/three.png"></img>
+            <img src="../assets/reactJS.png"></img>
+          </div>
+          <div className='github-logo'>
+             <h3>Source:</h3>
+            <a href='https://github.com/agan-k/aic-photography-selection'>
+               <img src='../assets/github.png' height='50' />
+            </a>
+          </div>
+
           <div className='screen-shots'>
              <div className='desktop'>
                <img src='../assets/screen_shots/aic_desktop.jpg'/>
@@ -50,8 +101,19 @@ export default function Landing() {
        </div>
        <div className='project katarina'>
          <h2>
-         <a href='https://katarina-olujic.netlify.app'>KatarinaOlujic.com</a>
-         </h2>
+            <a href='https://katarina-olujic.netlify.app'>KatarinaOlujic.com</a>
+          </h2>
+          <h3>Stack: ReactJS, CSS</h3>
+          <div className="logos small">
+            <img src="../assets/three.png"></img>
+            <img src="../assets/reactJS.png"></img>
+          </div>
+          <div className='github-logo'>
+             <h3>Source:</h3>
+            <a href='https://github.com/agan-k/katarinaolujic'>
+               <img src='../assets/github.png' height='50' />
+            </a>
+          </div>
           <div className='screen-shots'>
              <div className='desktop'>
                <img src='../assets/screen_shots/ko1.png'/>
@@ -64,8 +126,13 @@ export default function Landing() {
 
        <div className='project dom'>
          <h2>
-        DominiqueCarre.fr
+             DominiqueCarre.fr
          </h2>
+         <h3>Stack: HTML, CSS, jQuery</h3>
+          <div className="logos small">
+            <img src="../assets/three.png"></img>
+            <img src="../assets/jQuery.png"></img>
+          </div>
          <div className='screen-shots'>
             <div className='desktop'>
                 <img src='../assets/screen_shots/dom_desktop.jpg'/>
@@ -81,8 +148,11 @@ export default function Landing() {
        <div className="logos">
          <img src="../assets/three.png"></img>
          <img src="../assets/reactJS.png"></img>
+         <img src="../assets/nextJS.png"></img>
+         <img src="../assets/prismic-io.jpg"></img>
          <img src="../assets/nodeAndExpress.png"></img>
          <img src="../assets/postgresql.png"></img>
+         <img src="../assets/github.png"></img>
       </div>
        
   </div>
